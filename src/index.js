827c9a9511fc5e73d0156f30d7839f9c64ce832f
Juanpmodu92 <juanpmodu92@gmail.com>
2024-05-22 16:20:17 -0500

    Se creo la vista y el metodo para listar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /* -------------------------- Import -------------------------- */
 import express from 'express'
-
 import {join, dirname} from 'path'
 import {fileURLToPath} from 'url'
 import {engine} from 'express-handlebars';
 import morgan from 'morgan';
+import personasRoutes from './routes/personas.routes.js'
 
 /* ------------------------------- initialization ------------------------------ */
 const app = express();
@@ -31,6 +31,8 @@ app.use(express.json())
 app.get('/', (req, res)=> {
     res.render('index')
 });
+
+app.use(personasRoutes);
 
 /* ------------------------------ Public files ------------------------------ */
 app.use(express.static(join(__dirname, 'public')))
