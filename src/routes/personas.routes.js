c094259679de5a66ee81e4e5c58c7aadeba850e1
Juanpmodu92 <juanpmodu92@gmail.com>
2024-05-22 20:58:01 -0500

    Creamos la vista add, la ruta y el post del add

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -1,7 +1,25 @@
-import {Router} from 'express'
+import {Router, response} from 'express'
 import pool from '../database.js'
 
 const router = Router();
+
+router.get('/add', (req, res)=>{
+    res.render('personas/add')
+})
+
+
+router.post('/add', async(req, res)=>{
+    try {
+        const {name, lastname, age} = req.body
+        const newPersona = {
+            name, lastname, age 
+        }
+        await pool.query('INSERT INTO personas SET ?', [newPersona]);
+        res.redirect('/list');
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
 
 router.get('/list', async(req, res) => {
     try {
